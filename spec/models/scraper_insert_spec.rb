ceeb15e927acf4a21353d809a1eab4c78a447b8f
Charles Hudson <chudson@attinteractive.com>
2012-11-05 18:31:20 -0800

    Added a few more modifications and started testing

--- spec/models/scraper_insert_spec.rb
+++ spec/models/scraper_insert_spec.rb
@@ -1,11 +1,20 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require 'spec_helper'
 
 describe ScraperInsert do
+  fixtures :scraper_inserts
+
   before(:each) do
-    @scraper_insert = ScraperInsert.new
+    @scraper_insert = scraper_inserts(:one)
   end
 
   it "should be valid" do
     @scraper_insert.should be_valid
+  end
+
+  it "should validate required fields" do
+    @scraper_insert.regex = nil
+    @scraper_insert.content = nil
+
+    @scraper_insert.should_not be_valid
   end
 end
--- spec/models/scraper_spec.rb
+++ spec/models/scraper_spec.rb
@@ -1,11 +1,25 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require 'spec_helper'
 
 describe Scraper do
+  fixtures :scrapers
+
   before(:each) do
-    @scraper = Scraper.new
+    @scraper = scrapers(:one)
   end
 
   it "should be valid" do
     @scraper.should be_valid
+  end
+
+  it "should validate required fields" do
+    @scraper.title = nil
+    @scraper.url = nil
+    @scraper.description = nil
+
+    @scraper.should_not be_valid
+  end
+
+  it "should have an insert" do
+    @scraper.scraper_inserts.length.should be > 0
   end
 end
