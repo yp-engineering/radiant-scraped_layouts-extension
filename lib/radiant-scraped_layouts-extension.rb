2437dd5e580bec0f321d8a4341cae9410f136e1f
Josh Mervine <jmervine@yp.com>
2013-03-15 15:42:04 -0700

    updating version

--- lib/radiant-scraped_layouts-extension.rb
+++ lib/radiant-scraped_layouts-extension.rb
@@ -1,5 +1,5 @@
 module RadiantScrapedLayoutsExtension
-  VERSION     = "0.0.2"
+  VERSION     = "1.0.0"
   SUMMARY     = "Scraped Layouts for Radiant CMS"
   DESCRIPTION = "Generates scraped layouts, allowing content to be inserted via regular expressions."
   URL         = "https://github.com/yp-engineering/radiant-scraped_layouts-extension"
