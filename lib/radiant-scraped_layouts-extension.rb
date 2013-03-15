61a096e7fccc8e46b91b4256ff8fe44a014c2cc6
Josh Mervine <jmervine@yp.com>
2013-03-15 15:35:04 -0700

    updating URL and VERSION

--- lib/radiant-scraped_layouts-extension.rb
+++ lib/radiant-scraped_layouts-extension.rb
@@ -1,8 +1,8 @@
 module RadiantScrapedLayoutsExtension
-  VERSION     = "0.0.1"
+  VERSION     = "0.0.2"
   SUMMARY     = "Scraped Layouts for Radiant CMS"
   DESCRIPTION = "Generates scraped layouts, allowing content to be inserted via regular expressions."
-  URL         = "http://example.com/scraped_layout"
+  URL         = "https://github.com/yp-engineering/radiant-scraped_layouts-extension"
   AUTHORS     = ["Charles Hudson"]
   EMAIL       = ["charles.h.hudson@phobetron.com"]
 end
